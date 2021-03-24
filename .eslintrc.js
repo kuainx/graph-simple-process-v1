202e8c7669a7f99d839e229808533d070176fb56
KUAI <2542862169@qq.com>
2021-03-24 18:01:36 +0800

    init all use webstorm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,17 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		'plugin:vue/vue3-essential',
-		'plugin:vue/vue3-recommended',
-		'eslint:recommended',
-		'@vue/prettier',
-		'standard',
-	],
-	parserOptions: {
-		parser: 'babel-eslint',
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-tabs': 'off',
-		indent: ['error', 'tab'],
-		'comma-dangle': [
-			'error',
-			{
-				arrays: 'always-multiline',
-				objects: 'always-multiline',
-			},
-		],
-		semi: ['error', 'always'],
-	},
-};
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
