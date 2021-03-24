927d37d18956a7e58cbadb1a5f5df0fd9857c34a
KUAI <2542862169@qq.com>
2021-03-24 21:20:56 +0800

    step2

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,6 +6,7 @@
   </el-steps>
   <div class="content">
     <Step1 v-if="currentStep===0" @next="step1N"></Step1>
+    <Step2 v-if="currentStep===1" @next="step2N" :template="template"></Step2>
   </div>
   <el-button type="primary" round @click="nextStep">下一步</el-button>
 </template>
@@ -13,11 +14,12 @@
 <script>
 import {reactive, toRefs} from "vue";
 import Step1 from "@/components/Step1";
+import Step2 from "@/components/Step2";
 
 export default {
   name: "mainA",
   components: {
-    Step1,
+    Step1, Step2,
   },
   setup() {
     const state = reactive({
@@ -30,10 +32,15 @@ export default {
     const step1N = (e) => {
       state.currentStep = 1;
       state.template = e;
+    };
+    const step2N = (e) => {
+      state.currentStep = 2;
+      state.template = e;
       console.log(e);
     };
     return {
       step1N,
+      step2N,
       nextStep,
       ...toRefs(state),
     };
