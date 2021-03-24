0b9c3aa1270a38617ba77f59987e0dd89f2e8624
KUAI <2542862169@qq.com>
2021-03-24 20:04:19 +0800

    step1

---
+++ src/components/Step1.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-upload :auto-upload="false" drag action="" :multiple="false"
+             accept=".template" :show-file-list="false" :onChange="onFileSelected">
+    <i class="el-icon-set-up"></i>
+    <div class="el-upload__text">将文件拖到此处，或<em>点击选择文件</em></div>
+    <template #tip>
+      <div class="el-upload__tip">
+        请导入模板文件（.template）
+      </div>
+    </template>
+  </el-upload>
+  <el-dialog title="提示" v-model="dialogVisible" width="30%">
+    <div>确认选择该模板吗？</div>
+    <img class="thumb" :src="thumb" alt="">
+    <template #footer>
+    <span class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="confirmTemplate">确 定</el-button>
+    </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import {reactive, toRefs} from "vue";
+
+export default {
+  name: "Step1",
+  emits: ['next'],
+  setup(props, {emit}) {
+    let tempObj = null;
+    const state = reactive({
+      dialogVisible: false,
+      thumb: "",
+    });
+    const onFileSelected = (file) => {
+      if (!/\.template$/i.test(file.name)) {
+        this.$notify.error({
+          title: "错误",
+          message: "选择的不是.template文件",
+        });
+      } else {
+        const reader = new FileReader();
+        reader.readAsText(file.raw);
+        reader.onload = () => {
+          tempObj = JSON.parse(reader.result);
+          if (tempObj.thumb) {
+            state.thumb = tempObj.thumb;
+            state.dialogVisible = true;
+          } else {
+            this.$notify.error({
+              title: "错误",
+              message: "该template文件已损坏",
+            });
+          }
+        };
+      }
+    };
+    const confirmTemplate = () => {
+      emit("next", tempObj);
+      state.dialogVisible = false;
+    };
+    return {
+      onFileSelected,
+      confirmTemplate,
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.el-icon-set-up {
+  font-size: 67px;
+  color: #C0C4CC;
+  margin: 40px 0 16px;
+  line-height: 50px;
+}
+
+.thumb {
+  max-width: 100%;
+  max-height: 200px;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-steps :active="currentStep" align-center>
+    <el-step title="添加模板"></el-step>
+    <el-step title="上传资源"></el-step>
+    <el-step title="文件导出"></el-step>
+  </el-steps>
+  <div class="content">
+    <Step1 v-if="currentStep===0" @next="step1N"></Step1>
+  </div>
+  <el-button type="primary" round @click="nextStep">下一步</el-button>
+</template>
+
+<script>
+import {reactive, toRefs} from "vue";
+import Step1 from "@/components/Step1";
+
+export default {
+  name: "mainA",
+  components: {
+    Step1,
+  },
+  setup() {
+    const state = reactive({
+      currentStep: 0,
+      template: null,
+    });
+    const nextStep = () => {
+      state.currentStep++;
+    };
+    const step1N = (e) => {
+      state.currentStep = 1;
+      state.template = e;
+      console.log(e);
+    };
+    return {
+      step1N,
+      nextStep,
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  margin: 20px;
+  text-align: center;
+}
+</style>
